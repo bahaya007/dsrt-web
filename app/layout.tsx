aadc730624648aa8e8d3deb2ac7a0fcd7f30e329
DSRT OFFICIAL <dsrt.official71@gmail.com>
2025-08-08 14:30:55 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
 
+// app/layout.tsx
+import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
+
+export const metadata = {
+  title: 'DSRT Landing Page',
+  description: 'Digital Smart Revise Technology',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
