0e31e98bf3338661e080dd459c4f6cf0202f7005
DSRT OFFICIAL <dsrt.official71@gmail.com>
2025-08-08 21:43:04 +0700

    Update contrast.js

--- app/fitur/manual tools/js/contrast.js
+++ app/fitur/manual tools/js/contrast.js
@@ -1,15 +1,5 @@
-export function applyContrast(imageData, value) {
-  const data = imageData.data;
-  const cFactor = value / 100;
-
-  for (let i = 0; i < data.length; i += 4) {
-    data[i] = clamp(((data[i] - 128) * cFactor + 128), 0, 255);
-    data[i + 1] = clamp(((data[i + 1] - 128) * cFactor + 128), 0, 255);
-    data[i + 2] = clamp(((data[i + 2] - 128) * cFactor + 128), 0, 255);
-  }
-  return imageData;
-}
-
-function clamp(value, min, max) {
-  return Math.min(Math.max(value, min), max);
+// js/contrast.js
+export function applyContrast(ctx, img, value) {
+  ctx.filter = `contrast(${100 + value}%)`;
+  ctx.drawImage(img, 0, 0);
 }
