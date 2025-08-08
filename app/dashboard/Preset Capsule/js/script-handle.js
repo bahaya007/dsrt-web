8637717075512616f6433e33ed8531a2bda799a5
DSRT OFFICIAL <dsrt.official71@gmail.com>
2025-08-08 20:13:42 +0700

    script-handle.js

---
+++ app/dashboard/Preset Capsule/js/script-handle.js
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="css/basic/preset1.css" id="preset-style" />
+<img id="image-preview" src="your-image.jpg" class="preset1" alt="Preview" />
+<select id="preset-selector"></select>
